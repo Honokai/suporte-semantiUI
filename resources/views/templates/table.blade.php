1c9bbf3076397397d65148555b7aa722c04c792d
Honokai <emerson_hatred@hotmail.com>
2022-08-06 21:24:04 -0300

    alterando template layout, adicionando template para tableas

--- resources/views/templates/table.blade.php
+++ resources/views/templates/table.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $titulo ?? "Suporte"}}</title>
+    <title>{{ $titulo ?? 'Suporte' }}</title>
     <link rel="stylesheet" href="/css/semantic.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/custom.css">
     <script src="/js/jquery.min.js"></script>
     <script src="/js/semantic.min.js"></script>
     <script src="/js/semanticINIT.js"></script>
-
+    @livewireStyles
+    @powerGridStyles
 </head>
 <body>
     @isset($navbar)
@@ -95,5 +98,7 @@
         </div>
     @endisset
     @yield('conteudo')
+    @livewireScripts
+    @powerGridScripts
 </body>
 </html>
